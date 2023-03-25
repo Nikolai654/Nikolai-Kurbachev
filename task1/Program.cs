d206cebc3347153c9891b15d9a12d020475421e6
«Nikolai» <>
2023-03-25 17:06:23 +0300

    first commit

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Алгоритм на C# для вывода сообщения "Привет", если введенное число больше 7:
+int number;
+Console.WriteLine("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 7)
+{
+Console.WriteLine("Привет");
+}
---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Алгоритм на C# для вывода элементов числового массива, кратных 3:
+int[] numbers = { 1, 3, 5, 6, 9, 12, 15, 18 };
+
+foreach (int number in numbers)
+{
+if (number % 3 == 0)
+{
+Console.WriteLine(number);
+}
+}
